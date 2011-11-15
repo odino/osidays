a8547f032eb0773793ffb4b066ef9a4739b5196e
Alessandro Nadalin <alessandro.nadalin@gmail.com>
2011-11-15 22:55:17 +0100

    status 0

---
+++ README.md
@@ -0,0 +1,3 @@
+= OSIDAYS TDD Workshop
+
+Resolution of the shortest path problems, related to directed graphs, using Dijkstra's algorythm.
---
+++ src/Osidays/Algorithm/ShortestPath/Dijkstra.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Orient package.
+ *
+ * (c) Alessandro Nadalin <alessandro.nadalin@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Class Dijkstra
+ *
+ * @package     Osidays
+ * @subpackage  Algorithm
+ * @author      Alessandro Nadalin <alessandro.nadalin@gmail.com>
+ */
+
+namespace Osidays\Algorithm\ShortestPath;
+
+class Dijkstra
+{
+    
+}
+
